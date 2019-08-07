5d995807633ab1b41dfce0589858f4ba7e83cd61
newstar <kangsaebyeol@gmail.com>
2019-08-07 12:52:11 +0900

    Modify props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-function Rv(props) {
-  console.log(props)
-  return <h2>Her name is {props.name}.</h2>
+function Rv({ name }) {
+  return <h2>Her name is {name}.</h2>
 }
 
 function App() {
