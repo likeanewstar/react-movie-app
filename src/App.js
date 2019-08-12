12a13f42fe06004eb884d5c1132595b61d6a85d7
newstar <kangsaebyeol@gmail.com>
2019-08-12 23:27:50 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 
-function Song({ title }) {
-  return <h2>My favorite song is {title}.</h2>
+function Song({ title, albumart }) {
+  return <div>
+    <h2>My favorite song is {title}.</h2>
+    <img src={albumart} alt={title} />
+  </div>
+  
 }
+
+const SongILike = [
+  {
+    title: 'I',
+    art: 'http://image.yes24.com/momo/TopCate678/MidCate004/67730885.jpg'
+  },
+  {
+    title: 'Why',
+    art: 'http://image.yes24.com/momo/TopCate851/MidCate009/84865363.jpg'
+  },
+  {
+title: 'Fine',
+art: 'http://image.yes24.com/momo/TopCate1099/MidCate009/108618000.jpg'
+  },
+  {
+    title: 'Something New',
+    art: 'http://image.yes24.com/Goods/61508337/800x0'
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Song title="I" />
-      <Song title="Why" />
-      <Song title="Fine" />
-      <Song title="Something New" />
+      {SongILike.map(song => (
+        <Song title={song.title} albumart={song.art} />
+      ))}
     </div>
   );
 }
