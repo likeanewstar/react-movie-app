3027804774cfa18a4c94eb238a22819c6b771fe6
newstar <kangsaebyeol@gmail.com>
2019-08-12 23:39:47 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import React from 'react';
 
+const SongILike = [
+  {
+    id: 1,
+    title: 'I',
+    art: 'http://image.yes24.com/momo/TopCate678/MidCate004/67730885.jpg'
+  },
+  {
+    id: 2,
+    title: 'Why',
+    art: 'http://image.yes24.com/momo/TopCate851/MidCate009/84865363.jpg'
+  },
+  {
+    id: 3,
+title: 'Fine',
+art: 'http://image.yes24.com/momo/TopCate1099/MidCate009/108618000.jpg'
+  },
+  {
+    id: 4,
+    title: 'Something New',
+    art: 'http://image.yes24.com/Goods/61508337/800x0'
+  },
+];
+
 function Song({ title, albumart }) {
   return <div>
     <h2>My favorite song is {title}.</h2>
     <img src={albumart} alt={title} />
   </div>
-  
 }
-
-const SongILike = [
-  {
-    title: 'I',
-    art: 'http://image.yes24.com/momo/TopCate678/MidCate004/67730885.jpg'
-  },
-  {
-    title: 'Why',
-    art: 'http://image.yes24.com/momo/TopCate851/MidCate009/84865363.jpg'
-  },
-  {
-title: 'Fine',
-art: 'http://image.yes24.com/momo/TopCate1099/MidCate009/108618000.jpg'
-  },
-  {
-    title: 'Something New',
-    art: 'http://image.yes24.com/Goods/61508337/800x0'
-  },
-];
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
       {SongILike.map(song => (
-        <Song title={song.title} albumart={song.art} />
+        <Song key={song.id} title={song.title} albumart={song.art} />
       ))}
     </div>
   );
