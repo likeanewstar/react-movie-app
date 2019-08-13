c4168bf367197e1cd16caaf5b345bc793f497f13
newstar <kangsaebyeol@gmail.com>
2019-08-13 22:30:01 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SongILike = [
   {
     id: 1,
     title: 'I',
-    art: 'http://image.yes24.com/momo/TopCate678/MidCate004/67730885.jpg'
+    art: 'http://image.yes24.com/momo/TopCate678/MidCate004/67730885.jpg',
+    ratings: 5.0
   },
   {
     id: 2,
     title: 'Why',
-    art: 'http://image.yes24.com/momo/TopCate851/MidCate009/84865363.jpg'
+    art: 'http://image.yes24.com/momo/TopCate851/MidCate009/84865363.jpg',
+    ratings: 5.0
   },
   {
     id: 3,
 title: 'Fine',
-art: 'http://image.yes24.com/momo/TopCate1099/MidCate009/108618000.jpg'
+art: 'http://image.yes24.com/momo/TopCate1099/MidCate009/108618000.jpg',
+ratings: 5.0
   },
   {
     id: 4,
     title: 'Something New',
-    art: 'http://image.yes24.com/Goods/61508337/800x0'
+    art: 'http://image.yes24.com/Goods/61508337/800x0',
+    ratings: 5.0
   },
 ];
 
-function Song({ title, albumart }) {
+function Song({ title, albumart, ratings }) {
   return <div>
     <h2>My favorite song is {title}.</h2>
+    <h4>{ratings} / 5.0</h4>
     <img src={albumart} alt={title} />
   </div>
+}
+
+Song.propTypes = {
+  title: PropTypes.string.isRequired,
+  albumart: PropTypes.string.isRequired,
+  ratings: PropTypes.number.isRequired
 }
 
 function App() {
@@ -35,7 +47,7 @@ function App() {
     <div>
       <h1>Hello</h1>
       {SongILike.map(song => (
-        <Song key={song.id} title={song.title} albumart={song.art} />
+        <Song key={song.id} title={song.title} albumart={song.art} ratings={song.ratings} />
       ))}
     </div>
   );
