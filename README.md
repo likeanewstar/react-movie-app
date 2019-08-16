55cef57b79a379e6bc096cfa9b19b992ab8fc10c
newstar <kangsaebyeol@gmail.com>
2019-08-16 11:53:52 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Movie App
-
-React JS Foundamentals Course (2019 Update!)
---
+++ precache-manifest.808bdf0a4413d44d4e05595d498af8db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb9c2a7a1634797efcda7e8e73fdd90",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "35ad907328637fa5b053",
+    "url": "/react-movie-app/static/css/main.f97ea609.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/react-movie-app/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "35ad907328637fa5b053",
+    "url": "/react-movie-app/static/js/main.37a0377d.chunk.js"
+  },
+  {
+    "revision": "e0c3c7f83a8b3d0de128",
+    "url": "/react-movie-app/static/js/runtime~main.4dfe89d3.js"
+  }
+]);
